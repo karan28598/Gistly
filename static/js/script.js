0282d257b61ead000bc08adfc200857c7ebe4fc9
karan28598 <mr.karanagrawal@gmail.com>
2018-03-27 00:37:10 +0530

    add frontend

---
+++ static/js/script.js
@@ -0,0 +1,11 @@
+// 2. This code loads the IFrame Player API code asynchronously.
+var tag = document.createElement('script');
+
+
+tag.src = "https://www.youtube.com/iframe_api";
+var firstScriptTag = document.getElementsByTagName('script')[0];
+firstScriptTag.parentNode.insertBefore(tag, firstScriptTag);
+
+// grabs the text from the search box
+var val = document.getElementById("txtSearch").placeholder;
+
